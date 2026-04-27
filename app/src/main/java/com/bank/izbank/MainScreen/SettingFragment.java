4ea12acbef4d1ad172ee4b092acbc473a9133ef1
harsha-516 <harshamutyam56@gmail.com>
2026-04-27 23:37:05 +0530

    test commit

--- app/src/main/java/com/bank/izbank/MainScreen/SettingFragment.java
+++ app/src/main/java/com/bank/izbank/MainScreen/SettingFragment.java
@@ -13,7 +13,6 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
-
 import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
